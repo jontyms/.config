b7b4ad548878404e409a6b5540a28f056bee2793
JontyMS <jontyms@protonmail.com>
2021-12-15 20:39:06 -0500

    Initial commit Changes to be committed: 	new file:   p10k.zsh 	new file:   zshrc 	new file:   zshsetup.sh

---
+++ zshsetup.sh
@@ -0,0 +1,8 @@
+mv ./zshrc ~/.zshrc
+mv ./p10k.zsh ~/.p10k.zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+chsh -s /bin/zsh
